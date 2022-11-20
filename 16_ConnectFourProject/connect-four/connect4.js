b1d16bbbbdc69d7522f0ca51b518e98dbb93276f
Adi Wijaya Gani <adi@AWG-Macbook-Air.local>
2022-11-21 00:56:52 +0700

    update to exercise 16 Connect Four Project

--- 16_ConnectFourProject/connect-four/connect4.js
+++ 16_ConnectFourProject/connect-four/connect4.js
@@ -5,11 +5,11 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -17,33 +17,73 @@ var board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  let temp = [];
+    for (let i=0; i <= WIDTH-1; i++) {
+      temp.push(null);
+    }
+    for (let i=0; i <= HEIGHT-1; i++) {
+      board.push(temp);
+    }  
+  return board;
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+  htmlBoard = document.querySelector("#board")
 
-  // TODO: add comment for this code
-  var top = document.createElement("tr");
-  top.setAttribute("id", "column-top");
+  // Create HTML elements for the top row of the board
+  let top = document.createElement("tr"); //<tr></tr>
+  top.setAttribute("id", "column-top");   //<tr id="column-top"></tr>
   top.addEventListener("click", handleClick);
 
-  for (var x = 0; x < WIDTH; x++) {
-    var headCell = document.createElement("td");
-    headCell.setAttribute("id", x);
-    top.append(headCell);
+  for (let x = 0; x < WIDTH; x++) {
+    let headCell = document.createElement("td"); //<td></td>
+    headCell.setAttribute("id", x);              //<td id="x"></td>
+    top.append(headCell);                        
   }
+    // top:
+    // <tr id="column-top">
+    //   <td id="0"></td>
+    //   <td id="1"></td>
+    //   <td id="2"></td>
+    //   <td id="3"></td>
+    //   <td id="4"></td>
+    //   <td id="5"></td>
+    //   <td id="6"></td>
+    // </tr>
+
   htmlBoard.append(top);
 
-  // TODO: add comment for this code
-  for (var y = 0; y < HEIGHT; y++) {
-    const row = document.createElement("tr");
-    for (var x = 0; x < WIDTH; x++) {
-      const cell = document.createElement("td");
-      cell.setAttribute("id", `${y}-${x}`);
+  // Create HTML elements for the remaining rows of the board
+  for (let y = 0; y < HEIGHT; y++) {
+    const row = document.createElement("tr"); //<tr></tr>
+    for (let x = 0; x < WIDTH; x++) {
+      const cell = document.createElement("td"); //<td></td>
+      cell.setAttribute("id", `${y}-${x}`);      //<td id="0-0"></td>
       row.append(cell);
-    }
+    }   
+    //  <tr> --> row
+    //   <td id="0-0"></td> --> cell
+    //   <td id="0-1"></td>
+    //   <td id="0-2"></td>
+    //   <td id="0-3"></td>
+    //   <td id="0-4"></td>
+    //   <td id="0-5"></td>
+    //   <td id="0-6"></td>
+    //  </tr>
+    //  <tr>
+    //  <td id="1-0"></td>
+    //  <td id="1-1"></td>
+    //  <td id="1-2"></td>
+    //  <td id="1-3"></td>
+    //  <td id="1-4"></td>
+    //  <td id="1-5"></td>
+    //  <td id="1-6"></td>
+    // </tr>
+    // ...
+
     htmlBoard.append(row);
   }
 }
@@ -58,7 +98,23 @@ function findSpotForCol(x) {
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
-  // TODO: make a div and insert into correct table cell
+  // This function should add a div inside the correct td cell in the HTML game board. 
+  // This div should have the piece class on it, and should have a class for whether the current player is 1 or 2,
+  //  like p1 or p2.
+
+  //<div class="piece" class="p1">
+  //</div>
+  const newPiece =  document.createElement("div")
+  newPiece.classList.add("piece");
+  if (currPlayer === 1){
+    newPiece.classList.add("p1");  
+  }
+  else {
+    newPiece.classList.add("p2");  
+  }
+  
+  xy = document.getElementById(`${y}-${x}`)
+  xy.append(newPiece);
 }
 
 /** endGame: announce game end */
@@ -71,10 +127,10 @@ function endGame(msg) {
 
 function handleClick(evt) {
   // get x from ID of clicked cell
-  var x = +evt.target.id;
+  let x = +evt.target.id;
 
   // get next spot in column (if none, ignore click)
-  var y = findSpotForCol(x);
+  let y = findSpotForCol(x);
   if (y === null) {
     return;
   }
@@ -115,12 +171,12 @@ function checkForWin() {
 
   // TODO: read and understand this code. Add comments to help you.
 
-  for (var y = 0; y < HEIGHT; y++) {
-    for (var x = 0; x < WIDTH; x++) {
-      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
+      let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+      let vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      let diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+      let diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
 
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
